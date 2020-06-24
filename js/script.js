8cd30917b78241faff680f012e1086b686205731
NotLiu <aliu@lediur.com>
2020-06-23 20:36:06 -0500

    fixed mobile friendly for all phone sizes!

--- js/script.js
+++ js/script.js
@@ -4,12 +4,23 @@ window.addEventListener('load', function () {
 
     var page = window.location.href;
     var page_slice = page.slice(Math.max(page.length - 16, 1));
-    console.log(page);
-    console.log(page.slice(Math.max(page.length - 16, 1)));
+    console.log(w);
 
     if (!page_slice.includes("projects.html") && !page_slice.includes("experiences.html")) {
-        if (w < 500) {
+        if (w < 500 && w > 380) {
             document.getElementById("nav-bar").style.fontSize = "medium";
+            document.getElementById("aboutme").style.fontSize = "large";
+            document.getElementById("image-banner").getElementsByTagName("img")[0].style.height = "initial";
+            document.getElementById("image-banner").style.top = "initial";
+        }
+        else if (w <= 380 && w > 351) {
+            document.getElementById("nav-bar").style.fontSize = "small";
+            document.getElementById("aboutme").style.fontSize = "large";
+            document.getElementById("image-banner").getElementsByTagName("img")[0].style.height = "initial";
+            document.getElementById("image-banner").style.top = "initial";
+        }
+        else if (w <= 350) {
+            document.getElementById("nav-bar").style.fontSize = "x-small";
             document.getElementById("aboutme").style.fontSize = "large";
             document.getElementById("image-banner").getElementsByTagName("img")[0].style.height = "initial";
             document.getElementById("image-banner").style.top = "initial";
@@ -17,19 +28,41 @@ window.addEventListener('load', function () {
     }
     else if (page_slice.includes("projects.html")) {
 
-        if (w < 500) {
+        if (w < 500 && w > 380) {
             document.getElementById("nav-bar-nofix").style.fontSize = "medium";
             document.getElementById("projectblock").style.fontSize = "large";
         }
+        else if (w <= 380 && w > 351) {
+            document.getElementById("nav-bar-nofix").style.fontSize = "small";
+            document.getElementById("projectblock").style.fontSize = "large";
+        }
+        else if (w <= 350) {
+            document.getElementById("nav-bar-nofix").style.fontSize = "x-small";
+            document.getElementById("projectblock").style.fontSize = "large";
+        }
+
     }
     else {
-        document.getElementById("nav-bar-nofix").style.fontSize = "medium";
-        if (w < 500) {
+        if (w < 500 && w > 380) {
+            document.getElementById("nav-bar-nofix").style.fontSize = "medium";
             document.getElementById("experience-block").style.fontSize = "large";
             document.getElementById("experience-block").style.margin = "8% 17% 7%";
             document.getElementById("involvement-block").style.fontSize = "large";
             document.getElementById("involvement-block").style.margin = "4% 17%";
-
+        }
+        else if (w <= 380 && w > 351) {
+            document.getElementById("nav-bar-nofix").style.fontSize = "small";
+            document.getElementById("experience-block").style.fontSize = "Large";
+            document.getElementById("experience-block").style.margin = "8% 17% 7%";
+            document.getElementById("involvement-block").style.fontSize = "large";
+            document.getElementById("involvement-block").style.margin = "4% 17%";
+        }
+        else if (w <= 350) {
+            document.getElementById("nav-bar-nofix").style.fontSize = "x-small";
+            document.getElementById("experience-block").style.fontSize = "Large";
+            document.getElementById("experience-block").style.margin = "8% 17% 7%";
+            document.getElementById("involvement-block").style.fontSize = "large";
+            document.getElementById("involvement-block").style.margin = "4% 17%";
         }
     }
 
